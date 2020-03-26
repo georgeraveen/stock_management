51489c4f46a6174f0b340669bb3fd4acc72172fb
georgeraveen <georgeraveen@gmail.com>
2020-03-27 00:39:19 +0530

    a

--- api/server.js
+++ api/server.js
@@ -1,14 +1,20 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 const cors = require('cors');
+const path = require('path');
 
 const mongoose = require('mongoose');
 const config = require('./DB.js');
 const productRoute= require('./routes/product.route');
 const batchRoute= require('./routes/batch.route');
 
+app.use(express.static(path.join(__dirname, './../build')));
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+  
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB,{useNewUrlParser:true}).then(
     ()=>{console.log('Database is connected')},
