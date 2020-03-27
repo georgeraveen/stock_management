b79139f985680b6d9f02ccb228852f44429ba5ea
georgeraveen <georgeraveen@gmail.com>
2020-03-27 09:46:43 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -10,10 +10,10 @@ const config = require('./DB.js');
 const productRoute= require('./routes/product.route');
 const batchRoute= require('./routes/batch.route');
 
-app.use(express.static(path.join(__dirname, './../build')));
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+// app.use(express.static(path.join(__dirname, './../build')));
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, './../build', 'index.html'));
+//   });
   
 mongoose.Promise = global.Promise;
 mongoose.connect(config.DB,{useNewUrlParser:true}).then(
