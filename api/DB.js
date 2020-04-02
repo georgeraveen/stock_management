08bddc573b4b764460ac69d49fcdd22cac1fb68c
georgeraveen <georgeraveen@gmail.com>
2020-04-02 17:16:46 +0530

    Update DB.js

--- api/DB.js
+++ api/DB.js
@@ -1,4 +1,4 @@
 module.exports={
-    // DB:'mongodb+srv://user:user1234@cluster0-yfjuh.mongodb.net/stock_management'
-    DB: 'mongodb://localhost:27017/stock_management'
+    DB:'mongodb+srv://user:user1234@cluster0-yfjuh.mongodb.net/stock_management'
+    // DB: 'mongodb://localhost:27017/stock_management'
 }
